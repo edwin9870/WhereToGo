b311ec8203f4de9a84c9ba7db8ded9d9bed4fa8a
Edwin Ramirez Ventura <edwin9870@gmail.com>
2017-09-27 23:44:42 -0400

    Init project

--- app/src/main/java/com/edwin/android/wheretogo/event/EventActivity.kt
+++ app/src/main/java/com/edwin/android/wheretogo/event/EventActivity.kt
@@ -1,26 +1,14 @@
 package com.edwin.android.wheretogo.event
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import com.edwin.android.wheretogo.R
-import dagger.android.AndroidInjection
-import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.support.HasSupportFragmentInjector
-import javax.inject.Inject
 
-class EventActivity : AppCompatActivity(), HasSupportFragmentInjector, EventMVP.View {
+class EventActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var eventPresenter: EventMVP.Presenter
-
-    @Inject
-    lateinit var fragmentDispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event)
 
@@ -28,9 +16,5 @@ class EventActivity : AppCompatActivity(), HasSupportFragmentInjector, EventMVP.
         Log.d(this.classLoader.toString(), "Hello world $age")
     }
 
-    override fun supportFragmentInjector(): AndroidInjector<Fragment>? = fragmentDispatchingAndroidInjector
 
-    override fun setPresenter(presenter: EventMVP.Presenter) {
-
-    }
 }
